00f27af21f4f01f4cf1ce4c94ac0127481be03f5
driver1998 <driver1998.ms@outlook.com>
2017-03-31 01:01:08 +0800

    修改版本号为 Build 20170331

--- VMGuide.Core/Properties/AssemblyInfo.cs
+++ VMGuide.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("VMGuide Core")]
-[assembly: AssemblyDescription("Build 20170312")]
+[assembly: AssemblyDescription("Build 20170331")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VMGuide Core")]
